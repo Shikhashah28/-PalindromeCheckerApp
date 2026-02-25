6abcf7b2bc13071c869f01b73788ebdbc8d36450
naveen-sourc <nm4108@srmist.edu.in>
2026-02-25 09:18:31 +0530

    UC5:Reversed String

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,35 @@
+import java.util.Stack;
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    public static void main (String[]args){
-        System.out.println("system initialized successfully");
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+
+        Stack<Character> stack = new Stack<>();
+
+
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+
+        String reversed = "";
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+
+        if (input.equals(reversed)) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
